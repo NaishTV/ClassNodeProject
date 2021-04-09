03a1e2aceed56f8567c807bc855c6ca3f74a484b
deni3278 <deni3278@easv365.dk>
2021-04-09 14:48:16 +0200

    Add comments and JavaDocs
    Comments and JavaDocs have been added to the insert(value) and insertRecursive(Node parent, int value) methods.

--- src/main/java/bst/BinaryTree.java
+++ src/main/java/bst/BinaryTree.java
@@ -7,21 +7,38 @@ public class BinaryTree {
         root = null;
     }
 
+    /**
+     * Inserts a value into the binary search tree.
+     *
+     * @param value the value to be inserted into the tree
+     */
     public void insert(int value) {
         root = insertRecursive(root, value);
     }
 
+    /**
+     * Recursively calls itself comparing the parent node's value and the specified value until a possible leaf node has been reached.
+     * A new instance of Node with the specified value is created and returned, which represents the new leaf node.
+     * <p>
+     * If the value already exists in the tree, the recursion occurs without insertion.
+     *
+     * @param parent the current node
+     * @param value the value to be inserted into the tree
+     * @return either the new leaf node or the parent node
+     */
     private Node insertRecursive(Node parent, int value) {
+        // If the parent node is null, it is a possible leaf node and the value is inserted into it.
         if (parent == null) {
             return new Node(value);
         }
 
+        // Check whether the value to be inserted is less than or greater than the parent node's value.
         if (value < parent.value) {
-            parent.left = insertRecursive(parent.left, value);
+            parent.left = insertRecursive(parent.left, value);      // Insert recursively into the parent's left child.
         } else {
-            parent.right = insertRecursive(parent.right, value);
+            parent.right = insertRecursive(parent.right, value);    // Insert recursively into the parent's right child.
         }
 
-        return parent;
+        return parent;  // Return an instance of Node that contains the specified value, which represents a leaf node.
     }
 }
